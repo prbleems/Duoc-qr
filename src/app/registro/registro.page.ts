891042814d1ac5a6fd83ffbfa2bff3feffa396cd
Mateo <rayoman01@gmail.com>
2024-10-25 22:40:49 -0300

    Arreglo registro y Login
    Añadi que cuando te creas una cuenta te mande aviso que la cuenta se creo exitosamente y arregle el titulo de nombre a Usuario en login

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -17,42 +17,45 @@ export class RegistroPage implements OnInit {
   formularioregistro: FormGroup;
 
   constructor(public fb: FormBuilder,
-    public alertcontroller:AlertController) { 
+    public alertController: AlertController) {
     this.formularioregistro = this.fb.group({
-      'nombre':new FormControl("",Validators.required),
-      'password':new FormControl("",Validators.required),
-      'confirmacionpassword':new FormControl("",Validators.required)
+      'nombre': new FormControl("", Validators.required),
+      'password': new FormControl("", Validators.required),
+      'confirmacionpassword': new FormControl("", Validators.required)
     });
-
-
   }
 
   ngOnInit() {
   }
 
-  async guardar(){
+  async guardar() {
     var f = this.formularioregistro.value;
-    
-    
-    if(this.formularioregistro.invalid){
-      const alert = await this.alertcontroller.create({
-        header : 'Datos incompletos',
-        message : 'Tienes que llenar los datos',
-        buttons : ['aceptar']
+
+    if (this.formularioregistro.invalid) {
+      const alert = await this.alertController.create({
+        header: 'Datos incompletos',
+        message: 'Tienes que llenar los datos',
+        buttons: ['Aceptar']
       });
 
       await alert.present();
       return;
     }
+
     var usuario = {
-      nombre : f.nombre,
-      password : f.password
-    }
+      nombre: f.nombre,
+      password: f.password
+    };
 
-    localStorage.setItem('usuario',JSON.stringify(usuario));
+    localStorage.setItem('usuario', JSON.stringify(usuario));
 
-  
+    // Mostrar notificación de cuenta creada
+    const alert = await this.alertController.create({
+      header: '¡Cuenta creada!',
+      message: 'Tu cuenta ha sido creada exitosamente.',
+      buttons: ['Aceptar']
+    });
+
+    await alert.present();
   }
-
 }
-
